96d1c052a7aabe3e299498ebbc5636e999a2f694
mayorbobster <gillb4@telusplanet.net>
2016-06-22 22:58:04 -0600

    Create duoinfo.sh

---
+++ duoinfo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while [ 1 ]
+do
+./duo "`date` load:`uptime | awk '{print $9}' | sed 's/,//'`\nRPMS:`sensors | grep FAN | awk '{print $4}'`\nMEMFREE: `top -n 1 | head -4 | tail -1 | awk '{print $6}'` k\nPID %cpu Name       `ps aux --sort -c | head -2 | tail -1 | awk '{print $2" "$3" "$11}' | sed 's,/.*/,,'`\nCPU `sensors | grep "CPU T" | awk '{print $3}'`"
+sleep 1
+done
