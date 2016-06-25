b9b44ea05cdf3d6ecfab78f6f7cac547a557bb54
mayorbobster <gillb4@telusplanet.net>
2016-06-24 23:06:29 -0600

    Update duoinfo.sh

--- duoinfo.sh
+++ duoinfo.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+
+USER=`whoami`
+if [ $USER != 'root' ];
+then
+echo You must be root to run this script!
+exit
+fi
+
+
 while [ 1 ]
 do
-./duo "\c5`date` load:`uptime | awk '{print $8}' | sed 's/,//'`\n \c4RPMS:`sensors | grep FAN | awk '{print $4}'`\n \c3MEMFREE: `top -n 1 | head -4 | tail -1 | awk '{print $6}'` k\n \c7PID %cpu Name       `ps aux --sort -c | head -2 | tail -1 | awk '{print $2" "$3" "$11}' | sed 's,/.*/,,'`\n \c2CPU `sensors | grep "CPU T" | awk '{print $3}'`\n \n \c12\c20\c31\c46 \c5M\c6a\c7y\c1o\c2r \c3B\c4o\c5b\c6s\c7t\c1e\c2r"
+TODAY=`date`
+LOAD=`uptime | sed 's,^.*average: ,,' | sed 's/,.*//'`
+TOPROC=`ps aux --sort -c | head -2 | tail -1 | awk '{print $11}' | sed 's,^.*/,,'`
+TOPROCID="`ps aux --sort -c | head -2 | tail -1 | awk '{print $2" "$3}'`"
+FREEMEM=`top -n 1 | head -4 | tail -1 | awk '{print $6}'`
+CPU=`sensors | grep "CPU T" | awk '{print $3}'`
+RPMS=`sensors | grep FAN | awk '{print $4}'`
+BANNER="\c12\c20\c31\c46 \c5M\c6a\c7y\c1o\c2r \c3B\c4o\c5b\c6s\c7t\c1e\c2r"
+./duo "\c5`date` \caload:$LOAD\n\c4RPMS:$RPMS\n\c3MEMFREE: $FREEMEM k\n\c7PID %cpu Name\n\c6$TOPROCID $TOPROC\n\c2CPU $CPU\n\n$BANNER"
 sleep 1
 done
